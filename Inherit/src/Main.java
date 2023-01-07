baf7392cc1c8c5aa8aeb118d0c816560a2e853f7
PierreEdFils <93496035+PierreEdFils@users.noreply.github.com>
2023-01-07 15:20:07 -0500

    Update Fish in Animal Project for Inheritance

--- Inherit/src/Main.java
+++ Inherit/src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-       Mouse1 m1 = new Mouse1();
-       m1.leftClick();
-       m1.rightClick();
-       m1.scrollDown();
-       m1.scrollUp();
+       Dog d = new Dog("Loafer",1,1,50,250,2,4,1,40,"Silky Coat");
+       d.chew();
+       d.walk();
+       d.run();
+
+       Fish f = new Fish("jelly",1,1,20,10,5,2,5);
+       f.rest();
+       f.swim(8);
 
     }
 }
